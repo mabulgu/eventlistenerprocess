d3891fc7b0881fa8f62b3d61c693f8d0207330d3
pamAdmin <>
2020-05-31 14:12:40 +0300

    {/src/main/java/com/redhat/pam/templates/Message.java}

--- src/main/java/com/redhat/pam/templates/Message.java
+++ src/main/java/com/redhat/pam/templates/Message.java
@@ -7,11 +7,30 @@ package com.redhat.pam.templates;
 public class Message implements java.io.Serializable {
 
     static final long serialVersionUID = 1L;
-
-    public Message() {
+    
+    private int code;
+    private String text;
+     
+    public Message(int code, String text) {
+        super();
+        this.code = code;
+        this.text = text;
     }
-
-
-
+     
+    public int getCode() {
+        return code;
+    }
+     
+    public void setCode(int code) {
+        this.code = code;
+    }
+     
+    public String getText() {
+        return text;
+    }
+     
+    public void setText(String text) {
+        this.text = text;
+    }
 
 }
